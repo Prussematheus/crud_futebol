4d578ec1d8cc30ac5db3b06022fe0b30f447a498
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-08-25 09:58:04 -0300

    update - css iniciado

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 include("conexao.php");
-
+echo '<section id="times">';
 $sql = "SELECT * FROM times"; 
 $resultado = mysqli_query($conn, $sql);
 
@@ -14,6 +25,7 @@ while ($linha = mysqli_fetch_array($resultado)) {
     echo"<br><br>";
 }
 echo "<a href='cadastrar_time.php'>Cadastrar novo time</a><br><br>";
+echo'</section>';
 
 
 $sql = "SELECT * FROM jogadores";
